5f79be407c70d55fd7441a5b749d574939c8ad6d
Josh <josh@foolishproducts.com>
2018-07-27 12:28:31 -0400

    remeshing script

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 #include <igl/opengl/glfw/Viewer.h>
 
+#include <igl/decimate.h>
+#include <igl/upsample.h>
+
+void resample(Eigen::MatrixXd& V, Eigen::MatrixXi& F, int targetResolution)
+{
+	Eigen::MatrixXd Vcurr = V;
+	Eigen::MatrixXi Fcurr = F;
+	while (Fcurr.rows() < targetResolution * 2)
+	{
+		Eigen::MatrixXd Vtmp = Vcurr;
+		Eigen::MatrixXi Ftmp = Fcurr;
+		igl::upsample(Vtmp, Ftmp, Vcurr, Fcurr);
+	}
+	Eigen::VectorXi J;
+
+	igl::decimate(Vcurr, Fcurr, targetResolution, V, F, J);
+	igl::writeOBJ("resampled.obj", V, F);
+}
+
 int main(int argc, char *argv[])
 {
   // Inline mesh of a cube
-  const Eigen::MatrixXd V= (Eigen::MatrixXd(8,3)<<
+  Eigen::MatrixXd V= (Eigen::MatrixXd(8,3)<<
     0.0,0.0,0.0,
     0.0,0.0,1.0,
     0.0,1.0,0.0,
@@ -12,7 +31,7 @@ int main(int argc, char *argv[])
     1.0,0.0,1.0,
     1.0,1.0,0.0,
     1.0,1.0,1.0).finished();
-  const Eigen::MatrixXi F = (Eigen::MatrixXi(12,3)<<
+  Eigen::MatrixXi F = (Eigen::MatrixXi(12,3)<<
     1,7,5,
     1,3,7,
     1,4,3,
@@ -25,6 +44,8 @@ int main(int argc, char *argv[])
     1,6,2,
     2,6,8,
     2,8,4).finished().array()-1;
+
+  resample(V, F, 1000);
 
   // Plot the mesh
   igl::opengl::glfw::Viewer viewer;
